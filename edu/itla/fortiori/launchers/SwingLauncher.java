299eb5c0fe2b8a5f2c53b03a93dabeb821f30ccf
Raydelto Hernandez <raydelto@gmail.com>
2012-04-04 12:54:51 -0400

    Adding first class to the project

---
+++ edu/itla/fortiori/launchers/SwingLauncher.java
@@ -0,0 +1,10 @@
+package edu.itla.fortiori.launchers;
+/*
+ * @author Raydelto Hernández
+ * Launcher class for Swing User Interface
+ */
+public class SwingLauncher {
+	public static void main(String[] args) {
+		System.out.println("This class is not implemented yet");
+	}
+}
